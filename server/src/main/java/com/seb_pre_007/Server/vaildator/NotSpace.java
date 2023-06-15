e48253db2616670e1dc7656804830e9e72afa984
HanJuYoung309 <juyg407@naver.com>
2023-06-15 14:07:01 +0900

    feat: 회원가입시 vaildation 추가

--- server/src/main/java/com/seb_pre_007/Server/vaildator/NotSpace.java
+++ server/src/main/java/com/seb_pre_007/Server/vaildator/NotSpace.java
@@ -1,2 +1,18 @@
-package com.seb_pre_007.Server.vaildator;public @interface NotSpace {
+package com.seb_pre_007.Server.vaildator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {NotSpaceValidator.class})
+public @interface NotSpace {
+
+    String message() default "공백이 아니어야 합니다";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
--- server/src/main/java/com/seb_pre_007/Server/vaildator/NotSpaceValidator.java
+++ server/src/main/java/com/seb_pre_007/Server/vaildator/NotSpaceValidator.java
@@ -1,2 +1,22 @@
-package com.seb_pre_007.Server.vaildator;public class NotSpaceValidator {
+package com.seb_pre_007.Server.vaildator;
+
+import org.springframework.util.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.annotation.Annotation;
+
+
+public class NotSpaceValidator implements ConstraintValidator<NotSpace, String> {
+
+
+    @Override
+    public void initialize(NotSpace constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return value==null || StringUtils.hasText(value);
+    }
 }
