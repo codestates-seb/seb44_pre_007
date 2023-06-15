06d27aa37718872a759b94e90f52a634b2540f42
Minhyeong Song <alsuddl25@gmail.com>
2023-06-15 10:48:05 +0900

    Github Actions / Code Deploy 를 이용한 BE CI-CD 설정(EC2-DB 연결 제외)

---
+++ server/src/main/java/com/seb_pre_007/Server/HelloController.java
@@ -0,0 +1,13 @@
+package com.seb_pre_007.Server;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
--- server/src/main/java/com/seb_pre_007/Server/ServerApplication.java
+++ server/src/main/java/com/seb_pre_007/Server/ServerApplication.java
@@ -3,7 +3,7 @@ package com.seb_pre_007.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
