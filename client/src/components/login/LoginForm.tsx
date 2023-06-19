04816c576b57bd59ee96aca544ea0443e48c26c9
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-06-19 22:38:47 +0900

    feat: fix OAuth logic

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -1,5 +1,4 @@
 import tw from 'tailwind-styled-components';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import EmailLogin from './EmailLogin';
 import GoogleLoginButton from './GoogleLoginButton';
 import AddtionalInfo from './AdditionalInfo';
@@ -14,16 +13,13 @@ flex flex-col items-center
 const StyledFormContainer = tw.div`
 bg-white rounded-md shadow-md py-10 px-6 flex flex-col justify-center items-center
 `;
-const clientId = import.meta.env.VITE_CLIENT_ID;
 
 export default function LoginForm() {
   return (
     <StyledWrapper>
       <MiniLogo />
       <StyledOAuthDiv>
-        <GoogleOAuthProvider clientId={clientId}>
-          <GoogleLoginButton />
-        </GoogleOAuthProvider>
+        <GoogleLoginButton />
       </StyledOAuthDiv>
       <StyledFormContainer>
         <EmailLogin />
--- client/src/components/signup/GoogleLoginButton.tsx
+++ client/src/components/signup/GoogleLoginButton.tsx
@@ -1,19 +1,16 @@
-import { useGoogleLogin } from '@react-oauth/google';
 import tw from 'tailwind-styled-components';
-import axios from 'axios';
 import GoogleIcon from '../../public/Icons/GoogleIcon';
 
 export default function GoogleLoginButton() {
-  const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      axios.post(import.meta.env.VITE_URL, tokenResponse.access_token);
-    },
-  });
+  const handleLogin = () => {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  };
   const StyledButton = tw.button`
   flex justify-center border border-buttonBorder rounded w-full bg-white py-2.5 text-[13px] hover:bg-buttonHover active:shadow-btnActive
   `;
+
   return (
-    <StyledButton type="button" onClick={() => login()}>
+    <StyledButton type="button" onClick={() => handleLogin()}>
       <GoogleIcon /> Sign up with Google
     </StyledButton>
   );
