79b6941ef378ff27652b145bebbc283fdc13c4cc
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-06-21 19:50:29 +0900

    feat: add social login settings

--- client/src/components/login/GoogleLoginButton.tsx
+++ client/src/components/login/GoogleLoginButton.tsx
@@ -3,7 +3,8 @@ import GoogleIcon from '../../public/Icons/GoogleIcon';
 
 export default function GoogleLoginButton() {
   const handleLogin = () => {
-    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+    // eslint-disable-next-line operator-linebreak
+    window.location.href = import.meta.env.VITE_LOGOUT_URL;
   };
   const StyledButton = tw.button`
   flex justify-center border border-buttonBorder rounded w-full bg-white py-2.5 text-[13px] hover:bg-buttonHover active:shadow-btnActive
