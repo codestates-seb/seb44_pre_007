631105b59d9a9c5f4937c447f22fd7e51116c85b
Minhyeong Song <alsuddl25@gmail.com>
2023-06-22 12:48:24 +0900

    refactor: TagService 메서드 설명 추가

--- server/src/main/java/com/seb_pre_007/Server/tag/repostiory/TagRepository.java
+++ server/src/main/java/com/seb_pre_007/Server/tag/repostiory/TagRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TagRepository  extends JpaRepository<Tag, Long> {
-
     Optional<Tag> findByTagName(String tagName);
-
 }
