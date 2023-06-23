f43476eb8b5e81e15aadefd40dc144b25113f427
HanJuYoung309 <juyg407@naver.com>
2023-06-23 14:44:12 +0900

    fix: 답변리스트정렬되는거 삭제

--- server/src/main/java/com/seb_pre_007/Server/question/controller/QuestionController.java
+++ server/src/main/java/com/seb_pre_007/Server/question/controller/QuestionController.java
@@ -100,14 +100,6 @@ public class QuestionController {
         questionResponseDto.setQuestionUserNickname(questionData.getQuestionUserNickname());
         questionResponseDto.setTagList(questionData.getTagList());
 
-        // 질문에 달린 답변의 최근 작성일 순서로 정렬
-        List<AnswerResponseDto> sortedList= questionResponseDto.getAnswerList().stream()
-                .sorted(Comparator.comparing(AnswerResponseDto::getAnswerUpdated).reversed())
-                .collect(Collectors.toList());
-
-         questionResponseDto.setAnswerList(sortedList);
-        // Question -> QuestionResponseDto 로 변환 (end)
-
         return new ResponseEntity(questionResponseDto, HttpStatus.OK);
     }
 
