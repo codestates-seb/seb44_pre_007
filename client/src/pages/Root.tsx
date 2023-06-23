b135ab1e64419909c1165e0fb5d92de01cd1f80e
JeanneLee57 <jinhwa.lee.57@gmail.com>
2023-06-23 12:50:07 +0900

    fix: delete unused lines

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,15 +1,10 @@
 import { Outlet, useSearchParams } from 'react-router-dom';
-import { useState } from 'react';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 
 function Root() {
-  const [showDropdown, setShowDropdown] = useState(false);
   const [searchParams] = useSearchParams();
   const token = searchParams.get('access_token');
-  const handleDropdown = () => {
-    setShowDropdown(!showDropdown);
-  };
 
   if (token) {
     localStorage.setItem('token', `Bearer ${token}`);
--- client/src/pages/Users.tsx
+++ client/src/pages/Users.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../components/header/Header';
 
 export default function Users() {
   return <Outlet />;
