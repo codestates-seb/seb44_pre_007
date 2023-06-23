d901a820850a2ae9932c28cd4ca67c0f8b4afea7
Ryu jisu <wltn1418@gmail.com>
2023-06-23 15:56:18 +0900

    refact: 코드 전체 리팩토링

--- client/src/components/question/Answer.tsx
+++ client/src/components/question/Answer.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { AnswerT } from '../../types/types';
 import useMovePage from '../../hooks/useMovePage';
-import { ContentDiv, FlexColJustifyEndDiv } from '../../styles/styles';
+import { ContentDiv, FlexColJustifyEndDiv, FlexJustifyBetweenDiv } from '../../styles/styles';
 import ActionSpan from '../edit/ActionSpan';
 import DateDiv from '../DateDiv';
 
@@ -19,7 +19,7 @@ function Answer({ data, user }: { data: AnswerT; user: string }) {
   return (
     <div className="w-[654px] flex flex-col py-4 border-b border-brgray">
       <ContentDiv dangerouslySetInnerHTML={{ __html: content }} />
-      <div className="flex justify-between text-[13px] my-4 pt-1">
+      <FlexJustifyBetweenDiv className="text-[13px] my-4 pt-1">
         <div className="flex gap-2">
           {nickName === user && <ActionSpan callback={goToEdit} action="Edit" />}
         </div>
@@ -27,7 +27,7 @@ function Answer({ data, user }: { data: AnswerT; user: string }) {
           {updated !== created && <DateDiv content="edited" date={updated} />}
           <DateDiv content="answered" date={created} />
         </FlexColJustifyEndDiv>
-      </div>
+      </FlexJustifyBetweenDiv>
       <div className="text-Link text-[13px] text-right">{nickName}</div>
     </div>
   );
